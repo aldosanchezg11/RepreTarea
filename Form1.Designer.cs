b12b406fc53793b11c16822c1012faecb6fa4327
aldosanchezg11 <122333336+aldosanchezg11@users.noreply.github.com>
2024-06-10 17:20:09 -0600

    Update Form1.Designer.cs

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,16 +1,9 @@
-﻿namespace RepreTarea
+namespace RepreTarea
 {
     partial class Form1
     {
-        /// <summary>
-        ///  Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        ///  Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -22,17 +15,16 @@
 
         #region Windows Form Designer generated code
 
-        /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
             this.PCT_CANVAS = new System.Windows.Forms.PictureBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.angleTextBox = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.renderButton = new System.Windows.Forms.Button();
+
             ((System.ComponentModel.ISupportInitialize)(this.PCT_CANVAS)).BeginInit();
             this.SuspendLayout();
+
             // 
             // PCT_CANVAS
             // 
@@ -41,13 +33,15 @@
             this.PCT_CANVAS.Size = new System.Drawing.Size(603, 333);
             this.PCT_CANVAS.TabIndex = 0;
             this.PCT_CANVAS.TabStop = false;
+
             // 
-            // textBox1
+            // angleTextBox
             // 
-            this.textBox1.Location = new System.Drawing.Point(317, 12);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(151, 27);
-            this.textBox1.TabIndex = 1;
+            this.angleTextBox.Location = new System.Drawing.Point(317, 12);
+            this.angleTextBox.Name = "angleTextBox";
+            this.angleTextBox.Size = new System.Drawing.Size(151, 27);
+            this.angleTextBox.TabIndex = 1;
+
             // 
             // label1
             // 
@@ -57,27 +51,41 @@
             this.label1.Size = new System.Drawing.Size(46, 20);
             this.label1.TabIndex = 2;
             this.label1.Text = "angle";
+
+            // 
+            // renderButton
+            // 
+            this.renderButton.Location = new System.Drawing.Point(500, 12);
+            this.renderButton.Name = "renderButton";
+            this.renderButton.Size = new System.Drawing.Size(75, 30);
+            this.renderButton.TabIndex = 3;
+            this.renderButton.Text = "Render";
+            this.renderButton.UseVisualStyleBackColor = true;
+            this.renderButton.Click += new System.EventHandler(this.RenderButton_Click);
+
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.renderButton);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.angleTextBox);
             this.Controls.Add(this.PCT_CANVAS);
             this.Name = "Form1";
             this.Text = "Form1";
+
             ((System.ComponentModel.ISupportInitialize)(this.PCT_CANVAS)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
-
         }
 
         #endregion
 
         private PictureBox PCT_CANVAS;
-        private TextBox textBox1;
+        private TextBox angleTextBox;
         private Label label1;
+        private Button renderButton;
     }
 }
